4db8e187ae10cf73a778130923cdb1a28871d3e9
David Liu <recalleverything@163.com>
2020-10-19 14:52:37 +0800

    append week1 assignment

---
+++ Week_01/HelloClassLoader.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class HelloClassLoader extends ClassLoader {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Class<?> klass = new HelloClassLoader().findClass("Hello");
+        Object o = klass.newInstance();
+        Method method = klass.getMethod("hello");
+        method.invoke(o);
+    }
+
+    @Override
+    protected Class<?> findClass(String name) {
+        InputStream resourceStream = this.getClass().getResourceAsStream("Hello.xlass");
+        try {
+            byte[] bytes = new byte[resourceStream.available()];
+            resourceStream.read(bytes);
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (255 - bytes[i]);
+            }
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
