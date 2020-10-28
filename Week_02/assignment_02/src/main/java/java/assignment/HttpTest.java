cb34df9313bcad3a3c3af4cb8f18bf0d9699f68f
David Liu <recalleverything@163.com>
2020-10-28 21:38:32 +0800

    append week02 assignments

---
+++ Week_02/assignment_02/src/main/java/java/assignment/HttpTest.java
@@ -0,0 +1,26 @@
+package java.assignment;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * HttpTest
+ *
+ * @author David Liu
+ */
+public class HttpTest {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        try {
+            Request request = new Request.Builder().url("http://localhost:8808/test").build();
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().string());
+        } finally {
+            client.clone();
+        }
+    }
+
+}
